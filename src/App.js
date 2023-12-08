73b0e1e81e2e127cfdae0247638c3f571fe3da02
Your Name <you@example.com>
2023-12-07 22:35:12 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import MovieList from './components/MovieList';
+import MovieAdd from './components/MovieAdd';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [fetchedMovies, setFetchedMovies] = useState([]);
+
+  useEffect(() => {
+    // Fetch movies from TMDb API
+    axios.get('https://api.themoviedb.org/3/movie/popular', {
+      params: {
+        api_key: 'c0a9750e00cffc2dab5fc345015020d5',
+      },
+    })
+      .then(response => setFetchedMovies(response.data.results))
+      .catch(error => console.error('Error fetching movies:', error));
+  }, []);
+
+  const handleAdd = (newMovie) => {
+    // Add the new movie to the movies state
+    setMovies([...movies, { id: Date.now(), title: newMovie }]);
+  };
+
+  const handleDelete = (movieId) => {
+    console.log('Deleting movie with ID:', movieId);
+    // Implement logic to delete the movie with the given ID
+    // For simplicity, let's filter out the movie with the specified ID
+    const updatedMovies = movies.filter(movie => movie.id !== movieId);
+    console.log('Updated movies:', updatedMovies);
+    setMovies(updatedMovies);
+  };
+
+  // Combine the fetched movies with the user-added movies
+  const allMovies = [...fetchedMovies, ...movies];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MovieList movies={allMovies} onDelete={handleDelete} />
+      <MovieAdd onAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;
